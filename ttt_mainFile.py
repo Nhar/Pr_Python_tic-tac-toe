eacc7d927b24e60ae938444a82c21be8dfeda305
KKKKK <kosiarzksrz@gmail.com>
2019-09-15 19:13:35 +0200

    Rev. 4 - Add additional method (match, new_board, display_board, legal_moves)

--- ttt_mainFile.py
+++ ttt_mainFile.py
@@ -40,3 +40,39 @@ def ask_number(question, low, high):
     while response not in range(low, high):
         response = int(input(question))
     return response
+
+def match():
+    """Ustal, czy pierwszy ruch należy do gracza, czy do komputera."""
+    go_first = ask_yes_no("Czy chcesz mieć prawo do pierwszego ruchu? (t/n): ")
+    if go_first == "t":
+        print("\nPierwszy ruch należy do ciebie.")
+        human = X
+        computer = O
+    else:
+        print("\nComputer wykonuje ruch jako pierwszy.")
+        computer = X
+        human = O
+    return computer, human
+
+def new_board():
+    """Utwórz nową planszę gry."""
+    board = []
+    for square in range(NUM_SQUARES):
+        board.append(EMPTY)
+    return board
+
+def display_board(board):
+    """Wyświetl planszę gry na ekranie."""
+    print("\n\t", board[0], "|", board[1], "|", board[2])
+    print("\t", "---------")
+    print("\t", board[3], "|", board[4], "|", board[5])
+    print("\t", "---------")
+    print("\t", board[6], "|", board[7], "|", board[8], "\n")
+
+def legal_moves(board):
+    """Utwórz listę prawidłowych ruchów."""
+    moves = []
+    for square in range(NUM_SQUARES):
+        if board[square] == EMPTY:
+            moves.append(square)
+    return moves
