01b9b8f99379264ccececf3ce46a1ecf6f2551a3
KKKKK <kosiarzksrz@gmail.com>
2019-09-15 20:14:54 +0200

    Rev. 7 Add additional method congrat_winner() Add method main() Add call function main()

--- ttt_mainFile.py
+++ ttt_mainFile.py
@@ -150,3 +150,35 @@ def next_turn(turn):
         return O
     else:
         return X
+		
+def congrat_winner(the_winner, computer, human):
+    """Pogratuluj zwycięzcy."""
+    if the_winner != TIE:
+        print(the_winner, "jest zwycięzcą!\n")
+    else:
+        print("Remis!\n")
+
+def main():
+    display_instruct()
+    computer, human = match()
+    turn = X
+    board = new_board()
+    display_board(board)
+
+    while not winner(board):
+        if turn == human:
+            move = human_move(board, human)
+            board[move] = human
+        else:
+            move = computer_move(board, computer, human)
+            board[move] = computer
+        display_board(board)
+        turn = next_turn(turn)
+
+    the_winner = winner(board)
+    congrat_winner(the_winner, computer, human)
+
+
+# rozpocznij program
+main()
+input("\n\nAby zakończyć grę, naciśnij klawisz Enter.")
